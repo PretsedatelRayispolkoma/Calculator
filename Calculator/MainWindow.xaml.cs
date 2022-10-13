48ee0f9fe19cca55c94f1e400a41e9f8d413a6ce
211925 <211925@1220-012-22>
2022-10-13 19:30:27 +0300

    Добавьте файлы проекта.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResultBox.Text = "0";
+        }
+
+        double result = 0;
+        char sign;
+
+        private void OneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ResultBox.Text == "0")
+                ResultBox.Text = "";
+            ResultBox.Text += e.Source.ToString().Last().ToString();
+            if (ResultBox.Text.Last() == ',')
+                PointBtn.IsEnabled = false;
+        }
+
+        private void PlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            result = Convert.ToDouble(ResultBox.Text);
+            sign = e.Source.ToString().Last();
+            ResultBox.Text = "0";
+            PlusBtn.IsEnabled = false;
+            MinusBtn.IsEnabled = false;
+            MultiplyBtn.IsEnabled = false;
+            DivisionBtn.IsEnabled = false;
+            PointBtn.IsEnabled = true;
+        }
+
+        private void EqualBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PlusBtn.IsEnabled = true;
+            MinusBtn.IsEnabled = true;
+            MultiplyBtn.IsEnabled = true;
+            DivisionBtn.IsEnabled = true;
+            PointBtn.IsEnabled = true;
+            switch (sign)
+            {
+                case '+':
+                    result += Convert.ToDouble(ResultBox.Text);
+                    ResultBox.Text = result.ToString();
+                    break;
+                case '-':
+                    result -= Convert.ToDouble(ResultBox.Text);
+                    ResultBox.Text = result.ToString();
+                    break;
+                case '*':
+                    result *= Convert.ToDouble(ResultBox.Text);
+                    ResultBox.Text = result.ToString();
+                    break;
+                case '/':
+                    if(ResultBox.Text == "0")
+                    {
+                        MessageBox.Show("You can't divide by zero");
+                        return;
+                    }
+                    result /= Convert.ToDouble(ResultBox.Text);
+                    ResultBox.Text = result.ToString();
+                    break;
+                default:
+                    MessageBox.Show("Error");
+                    break;
+            }
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ResultBox.Text = "0";
+            sign = ' ';
+            result = 0;
+            PlusBtn.IsEnabled = true;
+            MinusBtn.IsEnabled = true;
+            MultiplyBtn.IsEnabled = true;
+            DivisionBtn.IsEnabled = true;
+            PointBtn.IsEnabled = true;
+        }
+    }
+}
